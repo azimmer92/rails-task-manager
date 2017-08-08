3fff570ddf64b8c585113e206409d6e87a3f9429
Anina Zimmer <a.zimmer@jacobs-university.de>
2017-08-08 16:11:29 +0100

    rails task manager

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
+  # get 'tasks/index', to:
+
+  # get 'tasks/show'
+
+  # get 'tasks/new'
+
+  # get 'tasks/create'
+
+  # get 'tasks/edit'
+
+  # get 'tasks/update'
+
+  # get 'tasks/destroy'
+  resources :tasks
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Task.create(title: "hello", message: "Hello from Lisbon.")
+Task.create(title: "hi", message: "Hello from Munich.")
+
